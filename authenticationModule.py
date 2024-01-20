c1c0c860276fecfa5e9af5a127354d20a8df6809
mothish <mothishkumarofficial00@gmail.com>
2024-01-20 11:55:21 +0530

    authenticationModuleCompleation

--- authenticationModule.py
+++ authenticationModule.py
@@ -1,4 +1,9 @@
 import pyrebase
+import firebase_admin
+from firebase_admin import credentials, auth
+
+cred = credentials.Certificate("/home/mothish/Projects/miniproject/chatbotx-338c8-firebase-adminsdk-rpsht-a5226dd47b.json")
+firebase_admin.initialize_app(cred)
 
 firebaseConfig = {
    "apiKey": "AIzaSyCOLYfF47_xfkTEZy8X3A4hwUFaCyLCals",
@@ -11,62 +16,64 @@ firebaseConfig = {
    "measurementId": "G-YKGQFC8GNV"
 }
 firebase = pyrebase.initialize_app(firebaseConfig)
-auth = firebase.auth()
+auth_pyrebase = firebase.auth()
 
 def signUp(email,password,name):
    try:
       if len(password) < 8:
          return {"success":False ,"error":"Password is weak... Please Give more than 8 characters"}
       else:
-         new_user = auth.create_user_with_email_and_password(email,password)
-         auth.update_user(new_user["idToken"], {"display_name": name})
-         #auth.send_email_verification(new_user["idToken"])
-         return {"success":True ,"user":{"name": name}}   
+         new_user = auth_pyrebase.create_user_with_email_and_password(email,password)
+         uid = new_user["localId"]
+         user = auth.update_user(uid,display_name=name)
+         auth_pyrebase.send_email_verification(new_user["idToken"])
+         return {"success":True ,"user":name,"response_msg":"Successfully Signed Up"}   
    except Exception as e:
       error_message = str(e)
       if "INVALID_EMAIL" in error_message:
-         return {"success": False, "error": "Invalid email format. Please enter a valid email address."}
+         return {"success": False, "response_msg": "Invalid email format. Please enter a valid email address."}
       elif "EMAIL_EXISTS" in error_message:
-            return {"success": False, "error": "Email already registered. Please use a different email address."}
+            return {"success": False, "response_msg": "Email already registered. Please use a different email address."}
       else:
-         return {"success": False, "error":f"Something Went Wrong Please Try again!!{error_message}"}
+         return {"success": False, "response_msg":f"Something Went Wrong Please Try again!!{e}"}
   
 
 
 def signIn(email,password):
    try:
-      user = auth.sign_in_with_email_and_password(email,password)
-      return {"success":True ,"user":user}
-   except auth.AuthError as auth_error:
-        if "INVALID_PASSWORD" in str(auth_error):
-            return {"success": False, "error": "Invalid password.."}
-        elif "USER_NOT_FOUND" in str(auth_error):
-            return {"success": False, "error": "Invalid email.."}
+      user = auth_pyrebase.sign_in_with_email_and_password(email,password)
+
+      return {"success":True ,"user":user["displayName"],"response_msg":"Sucessfully Signed In"}
+   except Exception as e:
+        if "INVALID_LOGIN_CREDENTIALS" in str(e):
+            return {"success": False, "response_msg": "Invalid login credentials.."}
+        elif "INVALID_EMAIL" in str(e):
+           return {"success": False, "response_msg": "Invalid Email ID.."}
         else:
-            return {"success": False, "error":"Something Error in Login Credintials..."}
-   except Exception:
-      return {"success":False,"error":"Something Went Wrong Please Try again!!!"}
+           return {"success":False,"response_msg":"Something Went Wrong Please Try again!!!"}
+   
+      
    
 def signOut():
    try:
-      auth.signOut()
-      return {"success":True}
-   except auth.AuthError:
-        return {"success": False, "error":"You are Logged In.."}
+      auth_pyrebase.current_user = None
+      return {"success": True, "response_msg": "Successfully signed out."}
+
    except Exception:
-      return {"success":False,"error":"Something Went Wrong Please Try again!!!"}
+      return {"success":False,"response_msg":"Something Went Wrong Please Try again!!!"}
    
 def reset_password(email):
     try:
-        auth.send_password_reset_email(email)
+        auth_pyrebase.send_password_reset_email(email)
         return {"success": True}
-    except auth.AuthError as auth_error:
-        error_message = str(auth_error)
-        if "EMAIL_NOT_FOUND" in error_message:
-            return {"success": False, "error": "No User Found in this email. Please Check Email or Create New Account"}
+    except Exception as e:
+        if "EMAIL_NOT_FOUND" in str(e):
+            return {"success": False, "response_msg": "No User Found in this email. Please Check Email or Create New Account"}
+        elif "INVALID_EMAIL" in str(e):
+           return {"success": False, "response_msg": "Invalid Email ID.."}
         else:
-            return {"success": False, "error": "Domething error in Credintials Please Try again!!"}
-    except Exception:
-      return {"success":False,"error":"Something Went Wrong Please Try again!!!"}
+            return {"success":False,"response_msg":"Something Went Wrong Please Try again!!!"}
+
+      
 
 
--- test.py
+++ test.py
@@ -1,9 +1,75 @@
-from authenticationModule import signUp,signOut,signIn
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from authenticationModule import signUp, signIn, signOut, reset_password
 
+class AuthApp(App):
+    def build(self):
+        self.email_input = TextInput(hint_text='Email', multiline=False)
+        self.password_input = TextInput(hint_text='Password', password=True, multiline=False)
+        self.name_input = TextInput(hint_text='Name', multiline=False)
 
-response = signUp("tetedssl@gmail.com","12346789","Example")
+        self.signup_button = Button(text='Sign Up', on_press=self.signup)
+        self.signin_button = Button(text='Sign In', on_press=self.signin)
+        self.signout_button = Button(text='Sign Out', on_press=self.signout)
+        self.reset_password_button = Button(text='Reset Password', on_press=self.reset_password)
 
-if response["success"]:
-            print("Sign Up Successful. User:", response["user"])
-else:
-            print("Sign Up Failed. Error:", response["error"])
+        self.result_label = Label(text='', halign='center')
+
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+        layout.add_widget(self.email_input)
+        layout.add_widget(self.password_input)
+        layout.add_widget(self.name_input)
+        layout.add_widget(self.signup_button)
+        layout.add_widget(self.signin_button)
+        layout.add_widget(self.signout_button)
+        layout.add_widget(self.reset_password_button)
+        layout.add_widget(self.result_label)
+
+        return layout
+
+    def signup(self, instance):
+        email = self.email_input.text
+        password = self.password_input.text
+        name = self.name_input.text
+
+        response = signUp(email, password, name)
+
+        if response["success"]:
+            self.result_label.text = response["response_msg"]
+        else:
+            self.result_label.text = response["response_msg"]
+
+    def signin(self, instance):
+        email = self.email_input.text
+        password = self.password_input.text
+
+        response = signIn(email, password)
+
+        if response["success"]:
+            self.result_label.text = f"Welcome, {response['user']}!"
+        else:
+            self.result_label.text = response["response_msg"]
+
+    def signout(self, instance):
+        response = signOut()
+
+        if response["success"]:
+            self.result_label.text = response["response_msg"]
+        else:
+            self.result_label.text = response["response_msg"]
+
+    def reset_password(self, instance):
+        email = self.email_input.text
+
+        response = reset_password(email)
+
+        if response["success"]:
+            self.result_label.text = "Password reset email sent."
+        else:
+            self.result_label.text = response["response_msg"]
+
+if __name__ == '__main__':
+    AuthApp().run()
